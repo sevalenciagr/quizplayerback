1bbd6c82a1e5d20a2ceff906e8032e4f2f01d511
sevalenciagr <88304046+sevalenciagr@users.noreply.github.com>
2021-09-13 01:10:45 -0500

    Interfaces

---
+++ 3.Application/QuizPlayer.Application.Interfaces/IBaseApplication.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizPlayer.Application.Interfaces
+{
+    using System.Threading.Tasks;
+    using QuizPlayer.Domain.Entities.Responses;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IBaseApplication<T>
+    {
+     
+    }
+}
---
+++ 3.Application/QuizPlayer.Application.Interfaces/IGameApplication.cs
@@ -0,0 +1,40 @@
+﻿
+namespace QuizPlayer.Application.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using QuizPlayer.Domain.Entities;
+    using QuizPlayer.Domain.Entities.Responses;
+    public interface IGameApplication
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        GenericResponse<Game> PlayGame(Player player);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="answerQuestion"></param>
+        /// <returns></returns>
+        GenericResponse<AnswerQuestionDto> AnswerQuestion(AnswerQuestionDto answerQuestion);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="answerQuestion"></param>
+        /// <returns></returns>
+        GenericResponse<AnswerQuestionDto> ChooseQuestion(AnswerQuestionDto answerQuestion);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="answerQuestion"></param>
+        /// <returns></returns>
+        GenericResponse<AnswerQuestionDto> StopGame(AnswerQuestionDto answerQuestion);
+    }
+}
